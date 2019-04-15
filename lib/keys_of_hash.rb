3063344f24594b6b3e64bf5294c861794ec32814
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 03:49:56 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -10,6 +10,20 @@ class Hash
         end
       end
     end
-    array
+    return array
   end
 end
+
+class Hash
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end
+    end
+    return array
+  end
+end 
